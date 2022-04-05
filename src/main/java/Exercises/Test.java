f953b27745ea1bd7a0a6050a1961117ed6717f75
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-04-04 23:31:18 -0300

    SubArrayDivision

--- src/main/java/Exercises/Test.java
+++ src/main/java/Exercises/Test.java
@@ -1,16 +1,22 @@
 package Exercises;
 
-
+import java.util.Arrays;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        String horario = "07:05:45 PM";
-
-        String convertion = horario.substring(0,2);
-        int result = (Integer.parseInt(convertion) + 12);
-        String newHour = String.valueOf(result);
-        System.out.println(horario.replace(horario.substring(0,2), newHour));
+        List<Integer> s = Arrays.asList(1,2,1,3,2);
+        int d = 3;
+        int count = 0;
         
+        int sum = 1;
+        
+        sum += s.get(0+1);
+        if(sum == d) {
+        	count++;
+        }
+        
+        System.out.println(count);
         
     }
 }
