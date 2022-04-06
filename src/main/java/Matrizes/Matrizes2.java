7320fef9c57a658cc0cb570522bec8b33e9d0b1f
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-04-05 23:34:00 -0300

    Update Matrizes2.java

--- src/main/java/Matrizes/Matrizes2.java
+++ src/main/java/Matrizes/Matrizes2.java
@@ -1,6 +1,5 @@
 package Matrizes;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Matrizes2 {
@@ -10,16 +9,26 @@ public class Matrizes2 {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Informe o numero de elementos da matriz:");
 		int n = scan.nextInt();
-		
-		int[][] matriz = new int[n][n];
 
 		while (n != 0) {
 			
+			int[][] matriz = new int[n][n];
+			
 			for(int i = 0; i < n; i++) {
 				for(int j = 0; j < n; j++) {
-					System.out.println(Arrays.deepToString(matriz));
+					matriz[i][j] = 1 + Math.abs(i - j);
 				}
 			}
+			
+			for(int i = 0; i < n; i++) {
+				System.out.printf("%3d", matriz[i][0]);
+				for(int j = 1; j < n; j++) {
+					System.out.printf(" %3d", matriz[i][j]);	
+					
+				}
+				System.out.println();
+			}
+			System.out.println();
 			
 			
 			System.out.print("Informe o numero de elementos da matriz:");
