483b091b6138bf804a9c22b2550291d8b7a6f0ae
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-04-06 17:09:36 -0300

    Update MigratoryBirds.java

--- src/main/java/HackerrankAlgorithms/MigratoryBirds.java
+++ src/main/java/HackerrankAlgorithms/MigratoryBirds.java
@@ -1,34 +1,39 @@
 package HackerrankAlgorithms;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.ArrayList;
 
 public class MigratoryBirds {
 	
-	public static List<Integer> migratoryBirds(List<Integer> arr) {
+	public static int migratoryBirds(List<Integer> arr) {
 		List<Integer> nums =  new ArrayList<>();
 		List<Integer> numsArray = new ArrayList<>();
-		int count = 0;
 		int bigger = 0;
-		int smallerType = 0;
+		int countBigger = 0;
+		int count = 0;
 	
 		// Verificando os valores iguais dentro do array
 		for(int i = 0; i < arr.size(); i++) {
 			for(int j = 0; j < arr.size(); j++) {
 				if(arr.get(i) == arr.get(j)) {
 					count++;
-				}
-				
+				}	
 			}
+			nums.add(arr.get(i));
 			numsArray.add(count);
 			count = 0;
 		}
 		
+		for(int i = 0; i < numsArray.size(); i++) {
+			if(numsArray.get(i) >= bigger) {
+				bigger = numsArray.get(i);
+				countBigger++;
+			}
+		}
 		
-		return nums;
+		
+		return countBigger-1;
 	}
 	
 	public static void main(String[] args) {
