b7c171051c62183e38a5b75ea1d392322bb26978
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-04-07 09:20:47 -0300

    Compare String

--- src/main/java/Exercises/Test.java
+++ src/main/java/Exercises/Test.java
@@ -1,18 +1,10 @@
 package Exercises;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Test {
     public static void main(String[] args) {
-        Set<Integer> nums = new HashSet<Integer>();
-        nums.add(10);
-        nums.add(20);
-        nums.add(30);
-        nums.add(40);
-        nums.add(50);
-        
-        System.out.println();
-        
+       String nome1 = "Leonardo Rocha";
+       String nome2 = "Leonardo Rocha";
+       
+       System.out.println(nome1.compareTo(nome2));
     }
 }
