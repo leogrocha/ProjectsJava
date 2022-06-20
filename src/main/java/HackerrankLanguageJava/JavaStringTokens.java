8b8e4a8c9eec3102ed16e5c3ab69e892e9019ad1
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-06-20 00:10:58 -0300

    Estrutura de Dados

--- src/main/java/HackerrankLanguageJava/JavaStringTokens.java
+++ src/main/java/HackerrankLanguageJava/JavaStringTokens.java
@@ -8,22 +8,24 @@ public class JavaStringTokens {
 		ArrayList<String> strings = new ArrayList<>();
 		String caracteres = "";
 		
-		for(int i = 1; i < s.length(); i++) {
-			String caracter = s.substring(i-1, i);
+		for(int i = 0; i < s.length(); i++) {
+			//String caracter = s.substring(i-1, i);
 			
-			if((!caracter.equals(" ")) || (!caracter.equals("!")) || (!caracter.equals(","))
-					|| (!caracter.equals("?")) || (!caracter.equals("."))
-					|| (!caracter.equals("_")) || (!caracter.equals("'"))
-					|| (!caracter.equals("@"))) {
+			if(s.codePointAt(i) != 32 && s.codePointAt(i) != 33 && s.codePointAt(i) != 44
+					&& s.codePointAt(i) != 63 && s.codePointAt(i) != 46 && s.codePointAt(i) != 95
+					&& s.codePointAt(i) != 39 && s.codePointAt(i) != 64) {
 				caracteres = caracteres + s.charAt(i);
 			} else {
-				strings.add(caracter);
+				strings.add(caracteres);
 				caracteres = "";
 			}
 			
+//			System.out.println(s.charAt(i) + " = " + s.codePointAt(i));
 		}
 		
 		System.out.println(strings.size());
 		System.out.println(strings);
 	}
 }
+
+// regex - /[^a-zA-Z0-9]+/g
