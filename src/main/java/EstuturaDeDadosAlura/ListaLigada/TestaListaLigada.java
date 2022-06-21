8171194bb49e645e8e248493afadfb205a27b846
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-06-21 07:17:00 -0300

    Lista - parte 1

---
+++ src/main/java/EstuturaDeDadosAlura/ListaLigada/TestaListaLigada.java
@@ -0,0 +1,16 @@
+package EstuturaDeDadosAlura.ListaLigada;
+
+public class TestaListaLigada {
+	public static void main(String[] args) {
+		
+		ListaLigada lista = new ListaLigada();
+		
+		System.out.println(lista);
+		lista.adicionaNoComeco("mauricio");
+		System.out.println(lista);
+		lista.adicionaNoComeco("paulo");
+		System.out.println(lista);
+		lista.adicionaNoComeco("guilherme");
+		System.out.println(lista);
+	}
+}
