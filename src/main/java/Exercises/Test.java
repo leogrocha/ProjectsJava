035bfe5e7ebdcab17e20e985217ef334a7e4a72d
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-07-04 17:35:19 -0300

    Excecoes

--- src/main/java/Exercises/Test.java
+++ src/main/java/Exercises/Test.java
@@ -1,10 +1,20 @@
 package Exercises;
 
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
-       String nome1 = "Leonardo Rocha";
-       String nome2 = "Leonardo Rocha";
+       ArrayList<String> nomes = new ArrayList<String>();
        
-       System.out.println(nome1.compareTo(nome2));
+       nomes.add("Seu Madruga");
+       nomes.add("Chaves");
+       nomes.add("Dona Florinda");
+       nomes.add("Seu Barriga");
+       nomes.add("Professor Linguica");
+       
+       System.out.println(nomes);
+       
+       nomes.add(2, "Bruxa do 71");
+       System.out.println(nomes);
     }
 }
